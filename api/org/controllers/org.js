252d826d88fe58451ef5586f82949b486b40b47b
luongquangcuong <lqcuongnd@gmail.com>
2020-07-01 16:37:18 +0700

    test

--- api/org/controllers/org.js
+++ api/org/controllers/org.js
@@ -4,5 +4,34 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const _ = require("lodash");
+const moment = require("moment");
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+    /**
+     * get allOrg
+     */
+    async find(ctx) {
+        const orgServices = strapi.services.org;
+        const { _populate } = ctx.query;
+        const limitNumber = 50;
+        const _limit = ctx.query._limit >= limitNumber
+          ? limitNumber
+          : ctx.query._limit || limitNumber;
+        const query = _.omit(ctx.query, ["_populate"]);
+    
+        const entities = await orgServices.find({ ...query }, _populate);
+
+        return entities.map(entity => {
+            const org = sanitizeEntity(entity, {
+              model: strapi.models.org,
+            });
+            // if (org.chef && org.chef.email) {
+            //   delete org.avatar;
+            // }
+            return org;
+          });
+      
+      },
+};
